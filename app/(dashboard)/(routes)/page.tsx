2bfddc2edd6a4840f51bc37dae1377e981e4ab72
shaheer21-sys <65283693+shaheer21-sys@users.noreply.github.com>
2024-04-21 13:42:58 +0530

    dashboard route setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-700">
+        Hello world
+      </p>
+      <Button variant="destructive">
+        Hel
+      </Button>
+    </div>
+  );
+}
